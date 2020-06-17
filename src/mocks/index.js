5d7949b265e62048da3ac04348d9bf275636834f
valentinogagliardi <valentino@valentinog.com>
2020-06-17 11:41:33 +0200

    Add miragejs and mock data

---
+++ src/mocks/index.js
@@ -0,0 +1,21 @@
+/* ONLY FOR DEVELOPMENT! DON'T IMPORT IN PRODUCTION */
+const Series = require("time-series-data-generator");
+
+const from = "2020-01-01T16:30:41Z";
+const until = "2020-05-01T18:30:00Z";
+const interval = 43200;
+const keyName = "amount";
+
+const salesSeries = new Series({ from, until, interval, keyName });
+export const sales = salesSeries.gaussian({
+  mean: 360,
+  variance: 10,
+  decimalDigits: 0
+});
+
+const subscriptionsSeries = new Series({ from, until, interval, keyName });
+export const subscriptions = subscriptionsSeries.gaussian({
+  mean: 9,
+  variance: 5,
+  decimalDigits: 0
+});
