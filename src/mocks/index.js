68e61c6b212a287a213565e82b5b764588077de3
valentinogagliardi <valentino@valentinog.com>
2020-06-25 18:09:48 +0200

    Import time series only in development

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,21 +1,26 @@
-/* ONLY FOR DEVELOPMENT! DON'T IMPORT IN PRODUCTION */
-const Series = require("time-series-data-generator");
+export let sales;
+export let subscriptions;
 
-const from = "2020-01-01T16:30:41Z";
-const until = "2020-05-01T18:30:00Z";
-const interval = 43200;
-const keyName = "amount";
+if (process.env.NODE_ENV === "development") {
+  /* ONLY FOR DEVELOPMENT! DON'T IMPORT IN PRODUCTION */
+  const Series = require("time-series-data-generator");
 
-const salesSeries = new Series({ from, until, interval, keyName });
-export const sales = salesSeries.gaussian({
-  mean: 360,
-  variance: 10,
-  decimalDigits: 0
-});
+  const from = "2020-01-01T16:30:41Z";
+  const until = "2020-05-01T18:30:00Z";
+  const interval = 43200;
+  const keyName = "amount";
 
-const subscriptionsSeries = new Series({ from, until, interval, keyName });
-export const subscriptions = subscriptionsSeries.gaussian({
-  mean: 9,
-  variance: 5,
-  decimalDigits: 0
-});
+  const salesSeries = new Series({ from, until, interval, keyName });
+  sales = salesSeries.gaussian({
+    mean: 360,
+    variance: 10,
+    decimalDigits: 0
+  });
+
+  const subscriptionsSeries = new Series({ from, until, interval, keyName });
+  subscriptions = subscriptionsSeries.gaussian({
+    mean: 9,
+    variance: 5,
+    decimalDigits: 0
+  });
+}
