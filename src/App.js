0cccf78dccbd4e25fb04d822d1718e77b3499152
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-03-01 19:56:04 -0600

    adding context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import DashboardShell from "./features/Dashboard/DashboardShell";
+import DataContext from './context/DataContext';
 
-const App = () => {
-  return <DashboardShell />;
+const initialState = {
+  loading: true,
+  error: "",
+  salesTotal: 0,
+  subscriptionsTotal: 0,
+  data: [{ timestamp: new Date().toISOString(), amount: 0 }]
+};
+
+const App = () => {  
+  return (
+    <DataContext.Provider value={initialState}>
+      <DashboardShell />
+    </DataContext.Provider>
+  );
 };
 
 export default App;
