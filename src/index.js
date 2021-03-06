3f10a0885356892353ee9868b6e920de07f072e8
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-03-06 10:05:59 -0600

    removing last vestiges of redux in the project

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import App from "./App";
-import store from "./store";
-import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
