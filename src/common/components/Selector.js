9c7710af2d9145e410244c430c04868e73aaca81
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-03-06 09:40:49 -0600

    ret-conning the chevron onto the selector

--- src/common/components/Selector.js
+++ src/common/components/Selector.js
@@ -5,7 +5,7 @@ const selector = (props) =>  {
     return (
         <div>
             <label htmlFor="select-list">{props.label}</label>
-            <div>
+            <div className="field">
                 <select id="select-list" onChange={props.onSelectionChanged}>
                     <option value="">--</option>
                     {
@@ -15,6 +15,15 @@ const selector = (props) =>  {
                         </option>
                     ))}
                 </select>
+                <div className="chevron-wrapper flex">
+                    <svg
+                    className="chevron"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    >
+                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                </div>
           </div>
         </div>
     );
