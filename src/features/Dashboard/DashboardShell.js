bf467d128bdeb763701e63d39ed90a4bf6d80351
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-03-06 09:51:19 -0600

    convert DashboardShell to function component

--- src/features/Dashboard/DashboardShell.js
+++ src/features/Dashboard/DashboardShell.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Aside from "../../common/components/Aside";
 import ChartContainer from "./ChartContainer";
 import Layout from "../../common/components/Layout";
@@ -6,52 +6,30 @@ import Main from "../../common/components/Main";
 import SummaryContainer from "./SummaryContainer";
 import Selector from '../../common/components/Selector';
 
-class DashboardShell extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { selectedLabel: "" };
+const DashboardShell = (props) => {
 
-    this.handleSelectChange = this.handleSelectChange.bind(this);
-  }
+  const [selectedLabel, setSelectedLabel ] = useState('');
 
-  handleSelectChange(event) {
-    const selectedLabel = event.target.selectedOptions[0].label;
-    this.setState({ selectedLabel });
-  }
-
-  buildSelect() {
-    const optionsForSelect = [
-      { label: "Sales", value: `${process.env.REACT_APP_BASE_URL}/sales/` },
-      {
-        label: "Subscriptions",
-        value: `${process.env.REACT_APP_BASE_URL}/subscriptions/`
-      }
-    ];
-
-    return (
-      <Selector label='Please select a chart:' 
-      options={optionsForSelect} 
-      onSelectionChanged={this.handleSelectChange} />    
-    );
-  }
-
-  render() {
-    return (
-      <Layout>
-        <Aside>
-          <h2># Polly dashboard</h2>
-          {this.buildSelect()}
-        </Aside>
-        <Main>
-          <h1>
-            Welcome, <span className="bold">learner!</span>
-          </h1>
-          <SummaryContainer />
-          <ChartContainer selectedLabel={this.state.selectedLabel} />
-        </Main>
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <Aside>
+        <h2># Polly dashboard</h2>
+        <Selector label='Please select a chart:' 
+          options={[
+            { label: "Sales", value: `${process.env.REACT_APP_BASE_URL}/sales/` },
+            { label: "Subscriptions", value: `${process.env.REACT_APP_BASE_URL}/subscriptions/`}
+          ]} 
+          onSelectionChanged={event => setSelectedLabel(event.target.selectedOptions[0].label)} />    
+      </Aside>
+      <Main>
+        <h1>
+          Welcome, <span className="bold">learner!</span>
+        </h1>
+        <SummaryContainer />
+        <ChartContainer selectedLabel={selectedLabel} />
+      </Main>
+    </Layout>
+  );
 }
 
 export default DashboardShell;
